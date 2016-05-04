79e9f9171e0ca712cf6697fd5161520d61368aa4
pontus lindström <pontus lindstrm>
2016-05-04 23:19:07 +0200

    Jag tror inte det här med att bygga en väntkö är bra. Firebase är asynchront och det verkar inte gå att låsa en nod, vilket skulle kunna innebära ett race condition. Kör istället med att lista de bord som finns.

--- src/components/lobby.js
+++ src/components/lobby.js
@@ -1,32 +1,45 @@
 import React from 'react';
 
 var root = new Firebase("https://plump.firebaseio.com");
-
+var authData = root.getAuth();
+var loggedinuserRef = root.child('users').child(authData.uid);
+var waitinglistRef = root.child('waitinglist');
 class Lobby extends React.Component{
   constructor(props){
     super(props);
 
-    var authData = root.getAuth();
-    var userRef = root.child('users').child(authData.uid);
-    this.state = {username: '', userTotalScore: 0};
-    var self = this;
-    userRef.on("value", function(snapshot){
 
+    this.state = {username: '', userTotalScore: 0};
+  }
+
+  componentDidMount(){
+
+    var self = this;
+    loggedinuserRef.on("value", function(snapshot){
+        var userData = snapshot.val();
+        var newState = self.state;
+        newState.username = userData.displayname;
+        newState.userTotalScore = userData.totalscore;
+        self.setState(newState);
     });
     if(!authData){
       console.log('ej inloggad');
     } else {
-      console.log(authData.uid);
+      console.log("inloggad");
     }
-    const username = userRef.child('displayname');
-    this.state = {user: username};
-    console.log(this.state.userid)
+  }
+
+  componentWillUnmount(){
+    root.off();
+    loggedinuserRef.off();
   }
 
   render() {
     return (
       <div>
-        <p>{this.state.user}</p>
+        <p>{this.state.username}</p>
+        <p>Poäng: {this.state.userTotalScore}</p>
+
       </div>
     )
   }
