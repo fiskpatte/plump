370555607c28eef2b5343f6f1fcee11e65491ce9
Pontus Lindström <pontusss85@gmail.com>
2016-05-05 09:34:49 +0200

    Fixade så att man kan starta ett nytt game

--- src/components/lobby.js
+++ src/components/lobby.js
@@ -3,30 +3,39 @@ import React from 'react';
 var root = new Firebase("https://plump.firebaseio.com");
 var authData = root.getAuth();
 var loggedinuserRef = root.child('users').child(authData.uid);
-var waitinglistRef = root.child('waitinglist');
+var gamesRef = root.child('games');
+
 class Lobby extends React.Component{
   constructor(props){
     super(props);
-
-
-    this.state = {username: '', userTotalScore: 0};
+    const opengames = [];
+    this.state = {uid: '', username: '', userTotalScore: 0, openGames: opengames};
   }
 
   componentDidMount(){
-
     var self = this;
+
     loggedinuserRef.on("value", function(snapshot){
         var userData = snapshot.val();
         var newState = self.state;
+        newState.uid = snapshot.key();
         newState.username = userData.displayname;
         newState.userTotalScore = userData.totalscore;
         self.setState(newState);
     });
-    if(!authData){
-      console.log('ej inloggad');
-    } else {
-      console.log("inloggad");
-    }
+
+    gamesRef.orderByChild("gameState").equalTo("waitingForPlayers").on("value", function(snapshot){
+      var openGamesFromFirebase = snapshot.val();
+      // uppdatera this.state.openGames
+      const newOpenGames = [];
+      for(var gameIndex in openGamesFromFirebase){
+        newOpenGames.push(openGamesFromFirebase[gameIndex]);
+      }
+      var newState = self.state;
+      newState.openGames = newOpenGames;
+      self.setState(newState);
+    });
+
   }
 
   componentWillUnmount(){
@@ -34,14 +43,44 @@ class Lobby extends React.Component{
     loggedinuserRef.off();
   }
 
+  newGameButtonClicked(){
+    var self = this;
+      root.child('games').push({
+        "player1": self.state.uid,
+        "player2" : "",
+        "player3": "",
+        "player4": "",
+        "gameState" : "waitingForPlayers"
+      });
+  }
+
+  takeSlotButtonClick(){
+    console.log("Tar slot");
+  }
+
   render() {
     return (
       <div>
         <p>{this.state.username}</p>
         <p>Poäng: {this.state.userTotalScore}</p>
+        <button onClick={this.newGameButtonClicked.bind(this)}>NYTT SPEL</button>
 
-      </div>
-    )
+        <p>Öppna spel:</p>
+        {this.state.openGames.map((openGame, index) => (
+          <div key={index} className="openGame">
+            <p>Spelare 1: {openGame.player1}</p>
+            <div className="playerSlot">{openGame.player2 == "" ? <button onClick={this.takeSlotButtonClick.bind(this)}>Ta plats</button>
+                                    : openGame.player2}
+            </div>
+            <div className="playerSlot">{openGame.player3 == "" ? <button onClick={this.takeSlotButtonClick.bind(this)}>Ta plats</button>
+                                    : openGame.player3}
+            </div>
+            <div className="playerSlot">{openGame.player4 == "" ? <button onClick={this.takeSlotButtonClick.bind(this)}>Ta plats</button>
+                                    : openGame.player4}
+            </div>
+          </div>
+        ))}
+      </div>);
   }
 }
 
